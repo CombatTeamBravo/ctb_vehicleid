92c3fa6cdf4cd0cc5694637d2aa44fc59031ae57
marty <doc.brown.83@gmail.com>
2014-03-20 19:56:09 +0800

    - added callsign p3d - updated config.cpp and model.cfg to include new model

--- src/config.cpp
+++ src/config.cpp
@@ -31,4 +31,23 @@ class cfgVehicles
 		hiddenSelections[]= {"v_id0", "v_id1", "v_id2", "v_id_plate"};
 		hiddenSelectionsTextures[]= {"ctb_vehicleid_plate\data\letters\2_ca.paa", "ctb_vehicleid_plate\data\letters\1_ca.paa", "ctb_vehicleid_plate\data\letters\b_ca.paa", "#(argb,8,8,3)color(0.015,0.012,0.010,1.0,co)"};
 	};
+	class ctb_vehicle_csplate_tank_e: Land_Noticeboard_F
+	{
+		mapSize= "0.30";
+		author= "Marty [CTB]";
+		_generalMacro= "ctb_vehicle_csplate_tank_e";
+		scope= 2;
+		displayName= "CTB Vehicle Callsign Plate (Tank Even)";
+		destrType= "DestructNo";
+		model= "ctb_vehicleid_plate\ctb_vehicle_csplate_tank.p3d";
+		hiddenSelections[]= {"e0","e1","e2","e3","e4","e5","e6","e7","e8","e9"};
+		hiddenSelectionsTextures[]= {"ctb_vehicleid_plate\data\letters\e_ca.paa", "ctb_vehicleid_plate\data\letters\v_ca.paa", "ctb_vehicleid_plate\data\letters\e_ca.paa","ctb_vehicleid_plate\data\letters\n_ca.paa"};
+	};
+	class ctb_vehicle_csplate_tank_o: ctb_vehicle_csplate_tank_e
+	{
+		_generalMacro= "ctb_vehicle_csplate_tank_o";
+		displayName= "CTB Vehicle Callsign Plate (Tank Odd)";
+		hiddenSelections[]= {"o0","o1","o2","o3","o4","o5","o6","o7","o8"};
+		hiddenSelectionsTextures[]= {"ctb_vehicleid_plate\data\letters\o_ca.paa", "ctb_vehicleid_plate\data\letters\d_ca.paa", "ctb_vehicleid_plate\data\letters\d_ca.paa"};
+	}
 };
